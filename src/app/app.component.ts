b92ae468aecdadc074430d94d87cd3043533a9d3
Santiago Suarez Quiroz <suarezquiroz@gmail.com>
2017-10-28 13:17:32 -0500

    Callenge completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  constructor(private http: HttpClient) { }
+
+  quote = 'Chuck Norris took your quote to the center of the Sun';
+  api = 'http://api.icndb.com/jokes/random'
+
+  ngOnInit() {
+
+    setInterval(() => {
+      this.http.get(this.api)
+        .subscribe(
+        data => {
+          this.quote = data["value"]["joke"];
+        }
+        );
+    }, 6000);
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { Observable } from 'rxjs/Observable';
 
 import { AppComponent } from './app.component';
+
+//start:    2017-10-28T17:36:57.929Z
+//finish:   2017-10-28T18:10:20.068Z
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
